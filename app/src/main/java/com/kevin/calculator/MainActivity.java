049575a24ae644bd2ad3513b3b6b2014db3320c2
kevin <441325961@qq.com>
2016-09-02 18:03:21 +0800

    添加按钮背景。添加一些测试。

--- app/src/main/java/com/kevin/calculator/MainActivity.java
+++ app/src/main/java/com/kevin/calculator/MainActivity.java
@@ -1,13 +1,212 @@
 package com.kevin.calculator;
 
+import android.content.res.Configuration;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "MainActivity";
+
+    private Button btn0;
+    private Button btn1;
+    private Button btn2;
+    private Button btn3;
+    private Button btn4;
+    private Button btn5;
+    private Button btn6;
+    private Button btn7;
+    private Button btn8;
+    private Button btn9;
+    private Button btnAc;
+    private Button btnDel;
+    private Button btnAdd;
+    private Button btnSub;
+    private Button btnMul;
+    private Button btnDiv;
+    private Button btnPoint;
+    private Button btnEqual;
+
+    private TextView tvResult;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+        Log.e(TAG,"onCreate");
+    }
+
+    private void findViews() {
+        btn0 = (Button) findViewById(R.id.btn0);
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn3 = (Button) findViewById(R.id.btn3);
+        btn4 = (Button) findViewById(R.id.btn4);
+        btn5 = (Button) findViewById(R.id.btn5);
+        btn6 = (Button) findViewById(R.id.btn6);
+        btn7 = (Button) findViewById(R.id.btn7);
+        btn8 = (Button) findViewById(R.id.btn8);
+        btn9 = (Button) findViewById(R.id.btn9);
+
+        btnAc = (Button) findViewById(R.id.btnAc);
+        btnDel = (Button) findViewById(R.id.btnDel);
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnSub = (Button) findViewById(R.id.btnSub);
+        btnMul = (Button) findViewById(R.id.btnMul);
+        btnDiv = (Button) findViewById(R.id.btnDiv);
+        btnPoint = (Button) findViewById(R.id.btnPoint);
+        btnEqual = (Button) findViewById(R.id.btnEqual);
+
+        tvResult = (TextView)findViewById(R.id.tvResult);
+
+        btn0.setOnClickListener(this);
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+        btn4.setOnClickListener(this);
+        btn5.setOnClickListener(this);
+        btn6.setOnClickListener(this);
+        btn7.setOnClickListener(this);
+        btn8.setOnClickListener(this);
+        btn9.setOnClickListener(this);
+        btnAc.setOnClickListener(this);
+        btnDel.setOnClickListener(this);
+        btnAdd.setOnClickListener(this);
+        btnSub.setOnClickListener(this);
+        btnMul.setOnClickListener(this);
+        btnDiv.setOnClickListener(this);
+        btnPoint.setOnClickListener(this);
+        btnEqual.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn0:
+                tvResult.append("0");
+                break;
+            case R.id.btn1:
+                tvResult.append("1");
+                break;
+            case R.id.btn2:
+                tvResult.append("2");
+                break;
+            case R.id.btn3:
+                tvResult.append("3");
+
+                break;
+            case R.id.btn4:
+                tvResult.append("4");
+
+                break;
+            case R.id.btn5:
+                tvResult.append("5");
+                break;
+            case R.id.btn6:
+                tvResult.append("6");
+                break;
+            case R.id.btn7:
+                tvResult.append("7");
+                break;
+            case R.id.btn8:
+                tvResult.append("8");
+                break;
+            case R.id.btn9:
+                tvResult.append("9");
+                break;
+            case R.id.btnAc:
+                tvResult.setText("");
+                break;
+            case R.id.btnDel:
+                if(!TextUtils.isEmpty(tvResult.getText().toString())){
+                    tvResult.setText(tvResult.getText().toString().substring(0,tvResult.getText().toString().length()-1));
+                }
+                break;
+            case R.id.btnAdd:
+                tvResult.append("+");
+                break;
+            case R.id.btnSub:
+                tvResult.append("-");
+                break;
+            case R.id.btnMul:
+                tvResult.append("x");
+                break;
+            case R.id.btnDiv:
+                tvResult.append("/");
+                break;
+            case R.id.btnPoint:
+                tvResult.append(".");
+                break;
+            case R.id.btnEqual:
+                tvResult.append("=");
+                break;
+
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(TAG, "onStart");
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.e(TAG, "onRestart");
+
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.e(TAG, "onRestoreInstanceState");
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        Log.e(TAG, "onSaveInstanceState");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(TAG, "onStop");
+    }
+
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        Log.e(TAG, "onConfigurationChanged");
     }
 }
